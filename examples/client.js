6e2a49cf7cd6b1af2077f29f71e2bdceec24964c
Kurimizumi <harryjshipton@gmail.com>
2016-08-29 01:33:54 +0100

    Added an example

---
+++ examples/client.js
@@ -0,0 +1,49 @@
+//Calculate pi using the Leibniz formula
+//Import the index file (usually HoneybeeHive)
+var HoneybeeHive = require("../index.js");
+//Import the fs module in order to import the key
+var fs = require("fs");
+//Define various settings
+const PORT = 54321;
+const ADDRESS = "localhost";
+
+//Import key from filesystem
+var key = fs.readFileSync("public.pem", "utf8");
+
+//Define eventHandler in order to assign it later
+var eventHandler;
+
+//Define our submission handler, to handle what happens once we submit work
+var submitHandler = function(success) {
+  //Tell the client the status of our submission
+  console.log("Submission " + (success ? "succeeded" : "failed"));
+  //Request more work, and pass it to the work handler
+  eventHandler.request(workHandler);
+}
+//Define our work handler, to handle what happens when we receive work
+var workHandler = function(work) {
+  //Define our piSection variable, to store the part of pi we calculated
+  var piSection = 0;
+  //Define n for Leibniz's formula, and calculate current position in it
+  var n = 1 + (4*1000000000*work);
+  //Loop from 0 (incl) to 1000000000 (excl)
+  for(var i=0; i < 1000000000; i++) {
+    //Do the current pair of the series
+    piSection += (4/n)-(4/(n+2));
+    //Add 4 to n
+    n += 4;
+  }
+  //Submit the work and callback to the submitHandler
+  eventHandler.submit(submitHandler);
+}
+
+//Create the client, connecting to the server at ADDRESS:PORT using its public key
+HoneybeeHive.Honeybee(ADDRESS, PORT, key, function(evtHandler) {
+  //Set eventHandler to the eventHandler
+  eventHandler = evtHandler;
+  //Callback once we know the client is registered and ready
+  eventHandler.once("registered", function() {
+    //Request our first piece of work
+    eventHandler.request(workHandler);
+  });
+});
---
+++ examples/server.js
@@ -0,0 +1,79 @@
+//Calculate pi using the Leibniz formula
+//Import the index file (usually honeybee-hive)
+var HoneybeeHive = require("../index.js");
+//Import the fs module in order to import the key
+var fs = require("fs");
+//Define various setting constants
+const PORT = 54321; //Listening port
+const WORK_TIMEOUT = 25000000; //UNUSED: Timeout until a client is assumed to not be completing the work
+const SESSION_TIMEOUT = 30000; //Timeout until a client is assumed to not be responding on the current session
+const GROUP_MAX = 2; //Amount of clients required to submit data for the work to be classed as completed
+const FINISH_COUNT = 100; //Amount of jobs to schedule
+//Define counter for amount of work pieces created
+var workCounter = 0;
+//Define counter for how many times work has been verified
+var completeCounter = 0;
+
+//Read the PEM encoded private key from the file system
+var serverPrivateKey = fs.readFileSync("private.pem", "utf8");
+//Define pi variable to store pi for later
+var pi = 0;
+
+//Start the server
+var eventEmitter = HoneybeeHive.Hive(PORT, serverPrivateKey, WORK_TIMEOUT, SESSION_TIMEOUT);
+//Tell the user that the server has been started
+console.log("Server started");
+//Listen for create work events
+eventEmitter.on("create_work", function(callback) {
+  //If we have 100 jobs submitted
+  if(workCounter == FINISH_COUNT) {
+    //Tell the callback that there's no more work
+    callback(false);
+  } else {
+    //Tell the callback the work that we wish to create
+    //In this case, we just supply the counter
+    callback(workCounter);
+  }
+  //Increment the work counter
+  workCounter++;
+});
+
+//Listen for workgroup complete events
+eventEmitter.on("workgroup_complete", function(array, callback) {
+  //We loop through the array, and check if all of the data sets are equal.
+  //You can do your own validation here e.g. have a majority consensus or verify
+  //the output of each one yourself, looking for valid data (useful for things
+  //similar to cryptocurrency mining
+  for(var i = 0; i < array.length - 1; i++) {
+    //We now have the individual work submitted by the clients
+    //If the current value is not equal
+    if(array[i] !== array[i+1]) {
+      //Call the callback with a false javascript value (e.g. false, undefined, etc)
+      callback(false);
+      //Return to prevent further execution
+      return;
+    }
+  }
+  //Call the callback with the first value, since it equals all other values
+  callback(array[0]);
+});
+
+//Listen for datachunk formation events
+eventEmitter.on("new_datachunk", function(datachunk) {
+  //Add datachunk to pi
+  pi += datachunk;
+});
+
+
+
+
+/*
+var Pi=0;
+var n=1;
+for (i=0;i<=1000000000000;i++)
+{
+Pi=Pi+(4/n)-(4/(n+2))
+n=n+4
+}
+console.log(Pi);
+*/
